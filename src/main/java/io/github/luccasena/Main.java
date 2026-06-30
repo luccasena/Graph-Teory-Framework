02862516e31cba76540af1e0785b2683dd1712b7
Lucca Sena <161384749+luccasena@users.noreply.github.com>
2026-06-30 07:36:41 -0300

    test: BreadthFirstSearch method

--- src/main/java/io/github/luccasena/Main.java
+++ src/main/java/io/github/luccasena/Main.java
@@ -1,12 +1,14 @@
 package io.github.luccasena;
 
 
+import io.github.luccasena.models.BreadthFirstSearch;
+import io.github.luccasena.models.DeepResearch;
 import io.github.luccasena.models.Graph;
 import io.github.luccasena.models.Vertex;
 
 public class Main {
     static void main() {
-        Graph graph = new Graph(true);
+        Graph graph = new Graph();
 
         graph.addVertex(new Vertex("1"));
         graph.addVertex(new Vertex("2"));
@@ -16,28 +18,26 @@ public class Main {
         graph.addVertex(new Vertex("6"));
         graph.addVertex(new Vertex("7"));
 
-        graph.addEdge("1", "2");
+        BreadthFirstSearch bfs = new BreadthFirstSearch();
 
+        graph.addEdge("1", "2");
         graph.addEdge("2", "4");
         graph.addEdge("2", "3");
-
         graph.addEdge("3", "1");
-
         graph.addEdge("4", "3");
         graph.addEdge("4", "1");
-
         graph.addEdge("5", "6");
         graph.addEdge("5", "7");
-
         graph.addEdge("6", "2");
-
         graph.addEdge("7", "4");
         graph.addEdge("7", "6");
 
-        graph.printMatrix();
-        IO.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-        graph.printVertices();
+        Vertex origem = graph.getVerticeByLabel("1");
+        Vertex destino = graph.getVerticeByLabel("7");
+        bfs.executarBFS(graph, origem);
+        bfs.imprimirCaminho(destino);
 
 
     }
 }
+
